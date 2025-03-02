50b4384206d51285908be02e89c2acab343fbeb0
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-03-02 13:37:05 +0400

    add routes

--- week-1/index2.js
+++ week-1/index2.js
@@ -1,21 +1,30 @@
 import http from 'http';
 
-// const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('HELLO WORD !!!');
+    } else if (req.url === '/login') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('LOGIN !!!');
+    }
+        if (req.url === '/about') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('About US !!!');
+        }
+})
+
+server.listen(4040, "localhost", () => {
+    console.log('Server is running on port http://localhost:4040');
+});
+
+
+
+// const server2 = http.createServer((req, res) => {
 //     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hello World\n');
+//     res.end('Welcome to my Node.js server !\n');
 // })
-//
-// server.listen(4040, "localhost", () => {
-//     console.log('Server is running on port http://localhost:4040');
-// });
-
-
-
-const server2 = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Welcome to my Node.js server !\n');
-})
-server2.listen(3000, "localhost", () => {
-    console.log("Server is running on port http://localhost:3000");
-})
+// server2.listen(3000, "localhost", () => {
+//     console.log("Server is running on port http://localhost:3000");
+// })
 
