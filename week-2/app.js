f7aff2a2c5c6fa164a5a9dc5bf59eb26bb0a9e55
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-03-06 00:59:09 +0400

    add backups logic

--- week-2/app.js
+++ week-2/app.js
@@ -23,8 +23,18 @@ app.get("/products", (req, res) => {
 //პროდუქტის დამატება
 app.post("/products", (req, res) => {
     const products = JSON.parse(data);
-    const newProduct = {...req.body, id: Date.now()};
+    const newProduct = {...req.body, id: Date.now(), createdAt: new Date().toISOString()};
     //Date.now იღებს იმ კონკრეტულ დროს,როცა შეიქმნა და იქნება უნიკალური და იმიტომ გვაწყობს ID-თ.
+
+
+    // Homework-იდან მე-4 დავალება
+    if (!newProduct.name || !newProduct.price) {
+        res.status(406).json({message: "name and price are required"})
+    }
+
+    //ბექაფის გაკეთება დამატებამდე
+    fs.copyFileSync("./data/products.json", `./data/products_backup_${newProduct.id}.json`);
+
     products.push(newProduct);
     fs.writeFileSync("./data/products.json", JSON.stringify(products));
     res.status(201).json(newProduct);
@@ -55,14 +65,22 @@ app.path("/products/:id", (req, res) => {
 //პროდუქტის წაშლა
 app.delete("/products/:id", (req, res) => {
     const products = JSON.parse(data);
+
+    // Create a backup before deleting თაიმსტამპით
+    const backupPath = `./data/products_backup_${Date.now()}.json`;
+
+    fs.writeFileSync(backupPath, JSON.stringify(products, null, 2));
+
+
     const newProducts = products.filter((product) => product.id !== parseInt(req.params.id));
+
     fs.writeFileSync("./data/products.json", JSON.stringify(newProducts));
-    res.status(200).send("Product deleted successfully.");
+    res.status(200).send("Product deleted successfully. Backup created.");
 })
 
 
 //stock-ი უნდა შემცირდეს 1-ით ყიდვის შემთხვევაში
-app.post("/buy/:id", (req, res) => {
+app.post("/products/buy/:id", (req, res) => {
     const productId = parseInt(req.params.id);
 
     const products = JSON.parse(data);
@@ -86,6 +104,8 @@ app.delete("/products", (req, res) => {
     fs.writeFileSync("./data/products.json", JSON.stringify([]));
     res.status(200).send("All products deleted successfully.");
 });
+
+
 
 
 app.listen(3000, () => {
