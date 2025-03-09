3f14cd3684d57244d694bd32082303d66ef12c33
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-03-09 21:23:38 +0400

    instal mongoose and connect to database-MongoDB

--- week-2/server.js
+++ week-2/server.js
@@ -1,6 +1,14 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_URL).then(() => {
+    console.log("Connected to the database-MongoDB!");
+}).catch((err) => {
+    console.error(err);
+})
 
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
