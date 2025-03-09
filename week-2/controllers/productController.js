a1400fdd4a5ff8737747e759de26e0874f8e4e2d
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-03-09 22:06:56 +0400

    add schema and model

--- week-2/controllers/productController.js
+++ week-2/controllers/productController.js
@@ -1,9 +1,22 @@
 import fs from "fs";
+import mongoose from "mongoose";
 const data = fs.readFileSync("./data/products.json", "utf8");
 
+const productSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    stock: { type: Number, required: true },
+    slug: { type: String },
+    createdAt: { type: Date, default: Date.now },
+})
+const Product = mongoose.model("Product", productSchema);
+
 //პროდუქტების გამოძახება
-const getProducts = (req, res) => {
-    res.json(JSON.parse(data));
+const getProducts = async (req, res) => {
+    const product = await Product.find({});
+    res.json(product);
 }
 
 //პროდუქტის დამატება
