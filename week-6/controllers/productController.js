95b5f4194f4bef038d93d93d262c851291188907
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-03-20 18:33:56 +0400

    install pg and connect with postgress

--- week-6/controllers/productController.js
+++ week-6/controllers/productController.js
@@ -1,17 +1,18 @@
-// const pool = require('./config/db.config');
+import pool from "../config/db.config.js";
+
 
 //Get all Products
 async function getProducts(req, res) {
 
     return res.json({message: 'Welcome to the Express PostgreSQL API'});
 
-    // try {
-    //     const result = await pool.query('SELECT * FROM Products');
-    //     res.json(result.rows);
-    // } catch (err) {
-    //     console.log('Error executing query', err.stack);
-    //     res.status(500).json({error: 'Internal server error'});
-    // }
+    try {
+        const result = await pool.query('SELECT * FROM products');
+        res.json(result.rows);
+    } catch (err) {
+        console.log('Error executing query', err.stack);
+        res.status(500).json({error: 'Internal server error'});
+    }
 }
 
 export {getProducts};
